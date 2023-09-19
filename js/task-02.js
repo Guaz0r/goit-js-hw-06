254b89498f13cc9615b8f31bf1547e555372f9a1
Guaz0r <skittles391@gmail.com>
2023-09-19 13:09:44 +0300

    begining

---
+++ js/task-02.js
@@ -0,0 +1,23 @@
+const ingredientsList = document.querySelector('#ingredients');
+
+const ingredients = [
+  'Potatoes',
+  'Mushrooms',
+  'Garlic',
+  'Tomatos',
+  'Herbs',
+  'Condiments',
+];
+
+
+ingredients.forEach((ingredient) => {
+  const listItem = document.createElement('li');
+  
+  listItem.textContent = ingredient;
+  
+  listItem.classList.add('item');
+  
+  ingredientsList.appendChild(listItem);
+});
+
+console.log(ingredientsList.innerHTML);
